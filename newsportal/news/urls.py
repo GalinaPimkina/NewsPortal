d4a47e503e41d0df1ee1e1013db677614ee40a1e
Galina <novikova-box@yandex.ru>
2023-12-11 14:45:56 +0300

    настроено постраницчное кеширование на главную страницу и страницу отображения одной новости

--- newsportal/news/urls.py
+++ newsportal/news/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
+from django.views.decorators.cache import cache_page
+
 from .views import *
 
 
 urlpatterns = [
     path('pages/', include('django.contrib.flatpages.urls')),
-
-    path('', PostList.as_view(), name='news'),
-    path('<int:pk>', PostText.as_view(), name='post_detail'),
+    path('', cache_page(60)(PostList.as_view()), name='news'),
+    path('<int:pk>', cache_page(300)(PostText.as_view()), name='post_detail'),
     path('search/', PostSearch.as_view(), name='post_search'),
     path('create/', PostCreate.as_view(), name='post_create'),
     path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
