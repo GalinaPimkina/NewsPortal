2dc4d7f4c9d05df85f3d1e55fddc78260bae86e5
Galina <novikova-box@yandex.ru>
2024-01-09 14:55:28 +0300

    бд изменена на postgreSQL

--- newsportal/newsportal/settings.py
+++ newsportal/newsportal/settings.py
@@ -96,9 +96,13 @@ WSGI_APPLICATION = 'newsportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
