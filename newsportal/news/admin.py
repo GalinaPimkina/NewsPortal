2c172e8b038e6297bf0f7d520aa6f3d85809ad7e
Galina <novikova-box@yandex.ru>
2024-01-09 14:23:46 +0300

    PostAdmin добавлен и зарегистрирован

--- newsportal/news/admin.py
+++ newsportal/news/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import *
+from .models import Category, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'time_create']
+    list_filter = ['author', 'time_create']
+
 
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
